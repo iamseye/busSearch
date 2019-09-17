a24e712c39e7b421b6ea7dfccbfbbc5137a6ea7d
Jess <iamseye@gmail.com>
2019-09-17 13:24:39 -0400

    ADD: ui in list card

--- src/components/listCard.js
+++ src/components/listCard.js
@@ -7,9 +7,36 @@ import '../styles/list.scss';
 const ListCard = () => (
   <Card className="listCard">
     <CardContent>
-      <Typography color="textSecondary" gutterBottom>
-        Departure
-      </Typography>
+      <div className="listCard__upper">
+        <div>
+          <Typography color="textSecondary" gutterBottom>
+            Greyhund
+          </Typography>
+        </div>
+        <div className="listCard__upper--price">
+          CAD $109
+        </div>
+      </div>
+
+      <div className="listCard__lower">
+        <div className="listCard__lower--schedule">
+          <Typography variant="body2" component="p">
+            Depart
+          </Typography>
+          <Typography variant="h5" component="h2">
+            11:45pm Montreal -- Gare d'autocars
+          </Typography>
+        </div>
+
+        <div className="listCard__lower--schedule">
+          <Typography variant="body2" component="p">
+            Arrive
+          </Typography>
+          <Typography variant="h5" component="h2">
+            9:05am New York -- Brooklyn Downtown
+          </Typography>
+        </div>
+      </div>
     </CardContent>
   </Card>
 );
